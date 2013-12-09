25ecbc187d9259eef18246bcff9678b36a4111e5
Arif De Sousa <arifads@gmail.com>
2013-12-09 08:51:01 -0430

    Initial Commit

---
+++ Estructuras Lineales/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Estructuras Lineales
+//
+//  Created by Arif De Sousa on 12/9/13.
+//  Copyright (c) 2013 ADS Studios. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
